75dd1daa8660e6b16b6d67d11d6c42ccb8bb64e7
DCCodyChaplin <cody.chaplin@dcmail.ca>
2022-04-01 17:26:46 -0400

    added project files from ICE11

--- Client/Scripts/app.ts
+++ Client/Scripts/app.ts
@@ -152,132 +152,19 @@
     {
         console.log("Contact-List Page");
 
-        if (localStorage.length > 0)
+        $("a.delete").on("click", function(event)
         {
-            let contactList = document.getElementById("contactList") as HTMLElement; // table body
-            let data = ""; // data container
-            let keys = Object.keys(localStorage); // s string[] of keys
-            let index = 1; // key count
-
-            for (const key of keys)
-            {
-                let contactData = localStorage.getItem(key); // get item associated with key
-
-                // create and deserialize contact
-                let contact = new core.Contact();
-                contact.deserialize(contactData as string);
-
-                // add html
-                data += `<tr>
-                <th scope="row" class="text-center">${index}</th>
-                <td>${contact.FullName}</td>
-                <td>${contact.ContactNumber}</td>
-                <td>${contact.EmailAddress}</td>
-                <td class="text-center">
-                    <button value="${key}" class="btn btn-primary btn-sm edit"><i class="fas fa-edit fa-sm"></i> Edit</button>
-                </td>
-                <td class="text-center">
-                    <button value="${key}" class="btn btn-danger btn-sm delete"><i class="fas fa-trash-alt fa-sm"></i> Delete</button>
-                </td>
-                </tr>`;
-                
-                index++; // increment counter
-            }
-
-            contactList.innerHTML = data; // add table row
-
-            // redirects to edit.html on button click, stores key in hash
-            $("button.edit").on("click", function()
-            {
-                location.href = "/edit#" + $(this).val() as string;
-            });
-
-            // removes item associated with key from localStorage and redirects to contact-list.html
-            $("button.delete").on("click", function()
-            {
-                if (confirm("Are you sure?"))
-                {
-                    localStorage.removeItem($(this).val() as string);
-                }
-
+            if (!confirm("Are you sure?"))
+            {   
+                event.preventDefault();
                 location.href = "/contact-list";
-            });
-        }
-
-        // redirects to edit.html on button click, stores "add" in hash
-        $("#addButton").on("click", () =>
-        {
-            location.href = "/edit#add";
+            }
         });
     }
 
     function DisplayEditPage()
     {
         ContactFormValidation();
-
-        let page = location.hash.substring(1);
-
-        switch(page)
-        {
-            case "add":
-                {
-                    // edits text/html on page
-                    $("main>h1").text("Add Contact");
-                    $("#editButton").html(`<i class="fas fa-plus-circle fe-lg"></i> Add`);
-
-                    // adds contact to localStorage
-                    $("#editButton").on("click", (event) =>
-                    {
-                        event.preventDefault();
-
-                        let fullName = document.forms[0].fullName.value as string;
-                        let contactNumber = document.forms[0].contactNumber.value as string;
-                        let emailAddress = document.forms[0].emailAddress.value as string;
-
-                        AddContact(fullName, contactNumber, emailAddress); // adds contact
-                        location.href = "/contact-list";
-                    })
-                    
-                    // redirect to contact-list.html
-                    $("#cancelButton").on("click", () =>
-                    {
-                        location.href = "/contact-list";
-                    })
-                }
-                break;
-            default:
-                {
-                    // creates Contact using deserialized data from localStorage
-                    let contact = new core.Contact();
-                    contact.deserialize(localStorage.getItem(page) as string);
-
-                    // updates values
-                    $("#fullName").val(contact.FullName);
-                    $("#contactNumber").val(contact.ContactNumber);
-                    $("#emailAddress").val(contact.EmailAddress);
-
-                    $("#editButton").on("click", (event) =>
-                    {
-                        event.preventDefault();
-
-                        // sets contact data using html values
-                        contact.FullName = $("#fullName").val() as string;
-                        contact.ContactNumber = $("#contactNumber").val() as string;
-                        contact.EmailAddress = $("#emailAddress").val() as string;
-
-                        localStorage.setItem(page, contact.serialize()); // updates record
-
-                        location.href = "/contact-list";
-                    });
-
-                    // redirect to contact-list.html
-                    $("#cancelButton").on("click", () =>
-                    {
-                        location.href = "/contact-list";
-                    })
-                }
-                break;
-        }
     }
 
     function CheckLogin()
@@ -392,14 +279,15 @@
                 DisplayServicesPage();
                 break;
             case "contact-list":
-                AuthGuard();
                 DisplayContactListPage();
                 break;
             case "contact":
                 DisplayContactPage();
                 break;
             case "edit":
-                AuthGuard();
+                DisplayEditPage();
+                break;
+            case "add":
                 DisplayEditPage();
                 break;
             case "login":
--- Routes/index.ts
+++ Routes/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 const router = express.Router();
 
+import Contact from '../Models/contact';
+
 /* GET home page. */
 router.get('/', function(req, res, next)
 {
@@ -54,12 +56,114 @@ router.get('/register', function(req, res, next)
 /* GET contact-list page. */
 router.get('/contact-list', function(req, res, next)
 {
-    res.render('index', { title: 'Contact List', page: 'contact-list', displayName: '' });
+    Contact.find(function(err, contactsCollection)
+    {
+        if (err)
+        {
+            console.error(err);
+            res.end(err);
+        }
+
+        res.render('index', { title: 'Contact List', page: 'contact-list', contacts: contactsCollection, displayName: '' });
+    });
 });
-/* GET edit page. */
-router.get('/edit', function(req, res, next)
+
+/* displays Add page */
+router.get('/add', function(req, res, next)
 {
-    res.render('index', { title: 'Edit', page: 'edit', displayName: '' });
+    res.render('index', { title: 'Add', page: 'edit', contact: '', displayName: '' });
+});
+
+/* process Add Request */
+router.post('/add', function(req, res, next)
+{
+    // instantiate new Contact
+    let newContact = new Contact
+    ({
+        "FullName": req.body.fullName,
+        "ContactNumber": req.body.contactNumber,
+        "EmailAddress": req.body.emailAddress
+    });
+
+    // insert contact into db
+    Contact.create(newContact, function(err: ErrorCallback)
+    {
+        if (err)
+        {
+            console.error(err);
+            res.end(err);
+        }
+
+        // newContact added successfully, redirect to contact-list
+        res.redirect('/contact-list');
+    });
+});
+
+/* displays Edit page */
+router.get('/edit/:id', function(req, res, next)
+{
+    let id = req.params.id;
+
+    //pass ID to db and read contact in
+    Contact.findById(id, {}, {}, function(err, contactToEdit)
+    {
+        if (err)
+        {
+            console.error(err);
+            res.end(err);
+        }
+
+        // show edit view with data
+        res.render('index', { title: 'Edit', page: 'edit', contact: contactToEdit, displayName: '' });
+    });
+
+});
+
+/* process Delete request*/
+router.get('/delete/:id', function(req, res, next)
+{
+    let id = req.params.id;
+
+    //pass ID to db and delete contact
+    Contact.remove({_id: id}, function(err)
+    {
+        if (err)
+        {
+            console.error(err);
+            res.end(err);
+        }
+
+        // delete successfull
+        res.redirect('/contact-list');
+    });
+});
+
+/* process Edit request*/
+router.post('/edit/:id', function(req, res, next)
+{
+    let id = req.params.id;
+
+    // instantiate new Contact
+    let updatedContact = new Contact
+    ({
+        "_id": id,
+        "FullName": req.body.fullName,
+        "ContactNumber": req.body.contactNumber,
+        "EmailAddress": req.body.emailAddress
+    });
+
+    // db.contacts.update
+    Contact.updateOne({_id: id}, updatedContact, function(err: ErrorCallback)
+    {
+        if (err)
+        {
+            console.error(err);
+            res.end(err);
+        }
+        
+        res.redirect('/contact-list');
+    });
+
 });
 
 export default router;
